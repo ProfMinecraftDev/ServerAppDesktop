9aedaf9d71be5121fdeb9bb0b64f96d7d9a30fe6
ProfMinecraftDev <profminecraftdev@gmail.com>
2025-12-21 19:15:45 -0400

    Implementación de AppWindow con HWND

--- src/ServerAppDesktop/MainWindow.xaml.cs
+++ src/ServerAppDesktop/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using ServerAppDesktop.Helpers;
+using System;
 using WinUIEx;
 
 namespace ServerAppDesktop
@@ -12,6 +15,19 @@ namespace ServerAppDesktop
         {
             InitializeComponent();
             ExtendsContentIntoTitleBar = true;
+
+            IntPtr hwnd = WindowHelper.GetWindowHandle();
+            if (hwnd != IntPtr.Zero)
+            {
+                var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+                var appWindow = AppWindow.GetFromWindowId(windowId);
+                var titleBar = appWindow.TitleBar;
+                titleBar.PreferredTheme = TitleBarTheme.UseDefaultAppMode;
+                titleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+                appWindow.SetIcon("Assets/AppIcon.ico");
+                appWindow.SetTaskbarIcon("Assets/AppIcon.ico");
+                appWindow.SetTitleBarIcon("Assets/AppIcon.ico");
+            }
         }
     }
 }
