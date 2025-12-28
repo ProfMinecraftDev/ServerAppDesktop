b7249dcfedd4fb2967a69c704170cd7bec3e7d19
ProfMinecraftDev <ProfMinecraftDev@gmail.com>
2025-12-28 18:43:10 -0400

    [Version 1.0.0.3 Preview] Implementación de controles personalizados y un nuevo converter

---
+++ src/ServerAppDesktop.Controls/InfoCard.cs
@@ -0,0 +1,56 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace ServerAppDesktop.Controls
+{
+    public sealed partial class InfoCard : Control
+    {
+        public InfoCard()
+        {
+            DefaultStyleKey = typeof(InfoCard);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(IconElement), typeof(InfoCard), new PropertyMetadata(null));
+        public IconElement Icon
+        {
+            get => (IconElement)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(InfoCard), new PropertyMetadata(string.Empty));
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof (InfoCard), new PropertyMetadata(string.Empty));
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            Padding = new Thickness(16);
+            CornerRadius = Application.Current.Resources.TryGetValue("ControlCornerRadius", out var cornerRadiusResource) && cornerRadiusResource is CornerRadius cornerRadius
+                ? cornerRadius
+                : new CornerRadius(4);
+            Background = Application.Current.Resources.TryGetValue("CardBackgroundFillColorDefaultBrush", out var backgroundResource) && backgroundResource is Brush backgroundBrush
+                ? backgroundBrush
+                : throw new InvalidOperationException("Unable to find background brush.");
+            BorderThickness = new Thickness(1);
+            BorderBrush = Application.Current.Resources.TryGetValue("CardStrokeColorDefaultBrush", out var borderBrushResource) && borderBrushResource is Brush borderBrush
+                ? borderBrush
+                : throw new InvalidOperationException("Unable to find border brush.");
+		}
+	}
+}
